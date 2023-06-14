536deda380f58c1f8da306950b2b94664789b1fe
Kara <blk20100@gmail.com>
2023-06-14 15:53:53 +0200

    Added CI GitHub Action #1

--- src/test/java/com/tfm/afac/AfacApplicationTests.java
+++ src/test/java/com/tfm/afac/AfacApplicationTests.java
@@ -3,7 +3,17 @@ package com.tfm.afac;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+//@ExtendWith(SpringExtension.class)
 @SpringBootTest
+//@TestPropertySource(locations = "classpath:test.properties")
+//@ActiveProfiles("dev")
 class AfacApplicationTests {
 
 	@Test

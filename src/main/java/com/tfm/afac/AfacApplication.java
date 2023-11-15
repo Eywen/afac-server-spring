ecb10297efb8c4d787c7a5ed183b92aa44bd6b80
Kara <blk20100@gmail.com>
2023-11-15 14:49:29 +0100

    clean code  Login #3

--- src/main/java/com/tfm/afac/AfacApplication.java
+++ src/main/java/com/tfm/afac/AfacApplication.java
@@ -1,7 +1,6 @@
 package com.tfm.afac;
 
-import com.tfm.afac.data.daos.UserRepository;
-import com.tfm.afac.data.model.Role;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,8 +12,6 @@ public class AfacApplication {
 
 		SpringApplication.run(AfacApplication.class, args);
 		System.out.println("pass encript: "+new BCryptPasswordEncoder().encode("secreto"));
-		System.out.println("pass encript: ");
-
 	}
 
 }
